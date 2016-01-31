cacc6b23ae073bc0e799e52b3c024052d7543b3c
bmldas132 <bimal@pixelstroke.com>
2016-01-31 21:24:04 +0530

    some

---
+++ Documents/Visual Studio 2015/Projects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10};
+
+            var result = numbers.Select(num => new { number = num })
+                            .Select(x => x.number);
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/MVCIdentityCustomizing/MVCIdentityCustomizing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIdentityCustomizing.MvcApplication" Language="C#" %>
